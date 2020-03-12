39292f9fd98d4f04e1438da2b3928319b80ae188
nick chen <nick.chen.pro@gmail.com>
2020-03-12 02:27:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,12 +18,12 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string){
-    return "I can't hear you";
+    return "I can't hear you!";
   }
   else if (string.toUpperCase() === string) {
     return 'YES INDEED!';
   }
   else if ('I love you, Grandma.' === string) {
-    return 'I love you, too';
+    return 'I love you, too.';
   }
 };
